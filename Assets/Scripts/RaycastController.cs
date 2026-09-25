ed794fc75517c356537c26c1d16b69dfc9c35a4d
MARÍA FERNANDA REYES PÉREZ <mariafernandareyper04@gmail.com>
2026-09-25 11:01:45 -0600

    Segunda parte del ejercicio de Raycast
    Aquí está la actualización de Raycast

---
+++ Assets/Scripts/RaycastController.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.EnhancedTouch;
+using System.Collections.Generic;
+using TMPro;
+
+public class RaycastController : MonoBehaviour
+{
+    [SerializeField] ARRaycastManager raycastManager;
+    [SerializeField] GameObject objectToPlace;
+    [SerializeField] TMP_Text debugText;
+
+    List<ARRaycastHit> hits = new List <ARRaycastHit>();
+
+    private void OnEnable()
+    {
+        EnhancedTouchSupport.Enable();
+    }
+
+    private void OnDisable()
+    {
+        EnhancedTouchSupport.Disable();
+    }
+    
+    void Update()
+    {
+        var activeTouches = UnityEngine.InputSystem.EnhancedTouch.Touch.activeTouches;
+
+        if(activeTouches.Count == 0)
+        {
+            debugText.text = "Esperando touch ...";
+            return;
+        }
+
+        var touch = activeTouches[0];
+
+        Debug.Log("TOUCH DETECTADO");
+        debugText.text = "TOUCH DETECTADO";
+
+        if (touch.phase != UnityEngine.InputSystem.TouchPhase.Began)
+            return;
+
+        if (raycastManager.Raycast(activeTouches[0].screenPosition, hits, TrackableType.PlaneWithinPolygon))
+        {
+            Debug.Log("HIT ENCONTRADO");
+
+            Pose hitPose = hits[0].pose;
+            debugText.text = "HIT: " + hitPose.position;
+            Instantiate(objectToPlace, hitPose.position, hitPose.rotation);
+        }
+
+        else
+        {
+            Debug.Log("NO SE ENCONTRO PLANO");
+            debugText.text = "NO HIT";
+        }
+
+    }
+}
